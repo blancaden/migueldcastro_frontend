13dde591c8a49f0a5aee0b4bffa4c3f00437dfd2
unknown <elisacapellinlopez@gmail.com>
2024-05-13 13:16:19 +0200

    continuamos estructurando HomeSection

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -9,48 +9,60 @@ const HomeSection = () => {
         <h2>Acompañamiento terapéutico, igualdad y desarrollo personal </h2>
 
         <Link to="/contacto">
-        <button>Contacto</button>
+          <button>Contacto</button>
         </Link>
       </div>
 
       <div className='HomeSection-Services'>
 
         <div className='Therapy'>
+          <Link to="/terapia">
+            <img src={imagen} alt="Terapia-Icono"/>
+          </Link>
           <h4>Terapia</h4>
           <p>La terapia es el acompañamiento a tu propio descubrir, déjame que te acompañe.</p>
 
           <Link to="/contacto">
-          <button>Contacto</button>
+            <button>Contacto</button>
           </Link>
 
         </div>
 
         <div className='Coaching'>
+          <Link to="/coaching">
+            <img src={imagen} alt="Coaching-Icono"/>
+          </Link>
           <h4>Coaching</h4>
           <p>Cada paso es una meta, cada avance es un logro en tu camino hacia el objetivo.</p>
 
           <Link to="/contacto">
-          <button>Contacto</button>
+            <button>Contacto</button>
           </Link>
 
         </div>
 
         <div className='Workshop'>
+          <Link to="/talleres">
+            <img src={imagen} alt="Talleres-Icono"/>
+          </Link>
           <h4>Talleres</h4>
           <p>Sumérgete en nuestros talleres para aprender y crecer juntos.</p>
 
           <Link to="/contacto">
-          <button>Contacto</button>
+            <button>Contacto</button>
           </Link>
 
         </div>
 
         <div className='Equality'>
+          <Link to="/igualdad">
+            <img src={imagen} alt="Igualdad-Icono"/>
+          </Link>
           <h4>Igualdad</h4>
           <p>Explora el mundo de la terapia sistémica y su poder transformador.</p>
 
           <Link to="/contacto">
-          <button>Contacto</button>
+            <button>Contacto</button>
           </Link>
 
         </div>
@@ -60,7 +72,7 @@ const HomeSection = () => {
           <p>Desde 2019 soy cofundador del Centro Vive, un centro dedicado al Desarrollo Personal, pensado en generar un  espacio donde poder acompañarte. Toda experiencia vivida hasta hoy me ha llevado a este presente, a este momento que me gustaría compartir contigo.
           </p>
           <Link to="/sobremi">
-          <button>Conóceme más</button>
+            <button>Conóceme más</button>
           </Link>
         </div>
 
