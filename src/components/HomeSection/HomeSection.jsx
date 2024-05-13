95ecfb76ebd1d67cc696680524d81491b85db612
unknown <elisacapellinlopez@gmail.com>
2024-05-13 12:54:21 +0200

    estructuración de HomeSection, sin estilos

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -1,10 +1,71 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const HomeSection = () => {
   return (
     <>
-   <h1>HOME SECTION</h1>
-   </>
+      <div className='HomeSection-Header'>
+        <h1>Miguel de Castro</h1>
+        <h2>Acompañamiento terapéutico, igualdad y desarrollo personal </h2>
+
+        <Link to="/contacto">
+        <button>Contacto</button>
+        </Link>
+      </div>
+
+      <div className='HomeSection-Services'>
+
+        <div className='Therapy'>
+          <h4>Terapia</h4>
+          <p>La terapia es el acompañamiento a tu propio descubrir, déjame que te acompañe.</p>
+
+          <Link to="/contacto">
+          <button>Contacto</button>
+          </Link>
+
+        </div>
+
+        <div className='Coaching'>
+          <h4>Coaching</h4>
+          <p>Cada paso es una meta, cada avance es un logro en tu camino hacia el objetivo.</p>
+
+          <Link to="/contacto">
+          <button>Contacto</button>
+          </Link>
+
+        </div>
+
+        <div className='Workshop'>
+          <h4>Talleres</h4>
+          <p>Sumérgete en nuestros talleres para aprender y crecer juntos.</p>
+
+          <Link to="/contacto">
+          <button>Contacto</button>
+          </Link>
+
+        </div>
+
+        <div className='Equality'>
+          <h4>Igualdad</h4>
+          <p>Explora el mundo de la terapia sistémica y su poder transformador.</p>
+
+          <Link to="/contacto">
+          <button>Contacto</button>
+          </Link>
+
+        </div>
+
+        <div className='HomeSection-KnowMe'>
+          <h3>Desde  mi adolescencia he creído en el potencial que cada uno de nosotros  lleva dentro. Como seres humanos somos completos y, por lo tanto,  capaces de hacer todo aquello que nos proponemos.</h3>
+          <p>Desde 2019 soy cofundador del Centro Vive, un centro dedicado al Desarrollo Personal, pensado en generar un  espacio donde poder acompañarte. Toda experiencia vivida hasta hoy me ha llevado a este presente, a este momento que me gustaría compartir contigo.
+          </p>
+          <Link to="/sobremi">
+          <button>Conóceme más</button>
+          </Link>
+        </div>
+
+      </div>
+    </>
   )
 }
 
