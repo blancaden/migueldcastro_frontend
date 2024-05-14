5a5b08e22267401ab0dc30cd41326a4b147b3225
Carolay Vasquez <151535611+carovasq@users.noreply.github.com>
2024-05-14 10:57:45 +0200

    add components: navbar, therapy, equality, coaching and aboutme

--- src/components/Coaching/Coaching.jsx
+++ src/components/Coaching/Coaching.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+// import React from 'react'
+import "./Coaching.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
-const Coaching = () => {
+function Coaching() {
   return (
     <>
-   <h1>COACHING</h1>
-   </>
-  )
+      <Container fluid className="coaching-container d-flex align-items-center">
+        <div>
+          <h1 className="m-5 coaching-h1">Coaching</h1>
+        </div>
+        <div>
+          <div className="d-flex">
+            <img src="" alt="" width="230px" height="230px" />
+            <div className="w-75 ms-5">
+              <h3>Individual</h3>
+              <p>
+                Toda acción comienza con un primer paso, cada uno que das es ya
+                de por si una meta, un logro, ya tienes hecho parte de tu
+                objetivo, ¿lo ves?, puedo acompañarte a visualizarlo, a que
+                emprendas las acciones necesarias para que sea una realidad, ¿me
+                lo permites? Es tu momento.
+              </p>
+              <button>
+                <i>+</i>Información
+              </button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
 
-export default Coaching
+export default Coaching;
--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,23 +1,19 @@
 import  {Outlet} from "react-router-dom";
-import Navbar from "../components/Navbar/Navbar";
+import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 
 
 const LayoutPublic = () => {
 
-        return(
-            <>
-                
-                <Navbar/>
-                
-                <Outlet/> 
+        return (
+          <>
+            <NavBar />
 
-                <Footer/>
-                
-                
-            </>
-    
-        )
+            <Outlet />
+
+            <Footer />
+          </>
+        );
     
     }
 
