2432ffd635766e389dea8ddeb89737f6de65e8c4
unknown <elisacapellinlopez@gmail.com>
2024-05-16 10:46:37 +0200

    HomeSection styles finished

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -10,12 +10,16 @@ const HomeSection = () => {
       <link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
 
       <div className='HomeSection-Header'>
-        <h1>Miguel de Castro</h1>
-        <h2>Acompañamiento terapéutico, igualdad y desarrollo personal </h2>
+        <div className='HomeSection-Title'>
+          <h1>Miguel de Castro</h1>
+          <h2>Acompañamiento terapéutico, igualdad y desarrollo personal </h2>
 
-        <Link to="/contacto">
-          <button>Contacto</button>
-        </Link>
+          <Link to="/contacto">
+            <button>Contacto</button>
+          </Link>
+        </div>
+
+        <img src="/img/Foto-Provisional-de-Miguel.svg" alt="Miguel de Castro" />
       </div>
 
       <img src="/img/Ondas.svg" alt="Ondas" />
@@ -24,7 +28,7 @@ const HomeSection = () => {
 
         <div className='Therapy'>
           <Link to="/therapy">
-          <img src="/img/Terapia-Icono.svg" alt="Terapia" />
+            <img src="/img/Terapia-Icono.svg" alt="Terapia" />
           </Link>
           <h4>Terapia</h4>
           <p>La terapia es el acompañamiento a tu propio descubrir, déjame que te acompañe.</p>
@@ -37,7 +41,7 @@ const HomeSection = () => {
 
         <div className='Coaching'>
           <Link to="/coaching">
-          <img src="/img/Coaching-Icono.svg" alt="Coaching" />
+            <img src="/img/Coaching-Icono.svg" alt="Coaching" />
           </Link>
           <h4>Coaching</h4>
           <p>Cada paso es una meta, cada avance es un logro en tu camino hacia el objetivo.</p>
@@ -50,7 +54,7 @@ const HomeSection = () => {
 
         <div className='Workshop'>
           <Link to="/workshop">
-          <img src="/img/Talleres-Icono.svg" alt="Talleres" />
+            <img src="/img/Talleres-Icono.svg" alt="Talleres" />
           </Link>
           <h4>Talleres</h4>
           <p>Sumérgete en nuestros talleres para aprender y crecer juntos.</p>
@@ -63,7 +67,7 @@ const HomeSection = () => {
 
         <div className='Equality'>
           <Link to="/equality">
-          <img src="/img/Igualdad-Icono.svg" alt="Igualdad" />
+            <img src="/img/Igualdad-Icono.svg" alt="Igualdad" />
           </Link>
           <h4>Igualdad</h4>
           <p>Explora el mundo de la terapia sistémica y su poder transformador.</p>
@@ -86,6 +90,8 @@ const HomeSection = () => {
           <button>Conóceme más</button>
         </Link>
       </div>
+
+      <img className='knowme-img' src="/img/Foto-Provisional-de-Miguel.svg" alt="Miguel de Castro" />
 
 
     </div>
