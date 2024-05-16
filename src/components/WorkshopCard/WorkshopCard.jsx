772934ff0b316e280418ed8e902478ba391d4571
Clara Ruiz-Santacruz Jiménez <clara@MacBook-Pro-de-Clara.local>
2024-05-16 14:03:21 +0200

    update

--- src/components/WorkshopCard/WorkshopCard.jsx
+++ src/components/WorkshopCard/WorkshopCard.jsx
@@ -13,7 +13,7 @@ const WorkshopCard = ({ talleres }) => {
    <link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       {talleres && talleres.map((talleres, index) => (
         <div key={index} className="workshop-card">
-          <Link to={`/talleres/${talleres.ID_Talleres}`} state={{ talleres }}>            
+          <Link to={`/taller/${talleres.ID_Talleres}`} state={{ talleres }}>            
             <div className='workshop-image-container'>
               <img
                 src={talleres.Imagen}
@@ -25,9 +25,11 @@ const WorkshopCard = ({ talleres }) => {
             </div>
           </Link>
           <div className="workshop-details-container">
-            <h3 className="workshop-title">{talleres.Titulo}</h3>
+            <div className="workshop-date-and-schedule-container">
             <p className="workshop-date">{talleres.Fecha}</p>
             <p className="workshop-schedule">{talleres.Horario}</p>
+            </div>
+            <h3 className="workshop-title">{talleres.Titulo}</h3>
             <p className="workshop-description">{talleres.Descripción}</p>
           </div>           
         </div>
