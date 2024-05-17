829af1312e3f68059dd8e6627fdd0dd2df532907
Lidia <ufonicap@gmail.com>
2024-05-17 12:09:19 +0200

    styles workshopdetail

--- src/components/WorkshopDetail/WorkshopDetail.jsx
+++ src/components/WorkshopDetail/WorkshopDetail.jsx
@@ -9,30 +9,25 @@ const WorkshopDetail = () => {
 
   return (
 
-    <>
-      <div className="workshopdetail-body">
-        <h1>ESTO ES WorkshopDetail</h1>^
-
-        <div className='workshop-image-container'>
-          <img
-            src={talleres.Imagen}
-            alt={talleres.Titulo}
-          />
-        </div>
-
-        <div className="workshop-details-container">
-          <div className="workshop-date-and-schedule-container">
-            <p className="workshop-date">{talleres.Fecha}</p>
-            <p className="workshop-schedule">{talleres.Horario}</p>
-          </div>
-          <h3 className="workshop-title">{talleres.Titulo}</h3>
-          <p className="workshop-description">{talleres.Descripción}</p>
-        </div>
+<>
+  <div className="workshopdetail-body-detail">
+    <h3 className="workshop-title-detail">{talleres.Titulo}</h3>
+    <div className="workshop-image-container-detail">
+      <img className='img-workshop-detail'
+        src={talleres.Imagen}
+        alt={talleres.Titulo}
+      />
+    </div>
+    <div className="workshop-details-container-detail">
+      <div className="workshop-date-and-schedule-container-detail">
+        <p className="workshop-date-detail">{talleres.Fecha}</p>
+        <p className="workshop-schedule-detail">{talleres.Horario}</p>
       </div>
+      <p className="workshop-description-detail">{talleres.Descripción}</p>
+    </div>
+  </div>
+</>
 
-
-
-    </>
   )
 }
 
