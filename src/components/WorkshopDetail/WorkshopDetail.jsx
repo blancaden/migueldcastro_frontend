8c7cffa48206f33043b9dc8bb2d9ddd4bf20a5ba
Lidia <ufonicap@gmail.com>
2024-05-17 13:29:00 +0200

    styles css of workshopdetail

--- src/components/WorkshopDetail/WorkshopDetail.jsx
+++ src/components/WorkshopDetail/WorkshopDetail.jsx
@@ -10,6 +10,9 @@ const WorkshopDetail = () => {
   return (
 
 <>
+
+<link href="https://fonts.googleapis.com/css2?family=Ovo&display=swap" rel="stylesheet" />
+   <link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
   <div className="workshopdetail-body-detail">
     <h3 className="workshop-title-detail">{talleres.Titulo}</h3>
     <div className="workshop-image-container-detail">
@@ -20,7 +23,11 @@ const WorkshopDetail = () => {
     </div>
     <div className="workshop-details-container-detail">
       <div className="workshop-date-and-schedule-container-detail">
+     
+      <img src="/img/calendar-icon.svg" alt="Miguel de Castro" />
+
         <p className="workshop-date-detail">{talleres.Fecha}</p>
+        <img className= 'watch-icon-detail'src="/img/watch-icon.svg" alt="Miguel de Castro" />
         <p className="workshop-schedule-detail">{talleres.Horario}</p>
       </div>
       <p className="workshop-description-detail">{talleres.Descripción}</p>
