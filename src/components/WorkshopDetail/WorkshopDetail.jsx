95243c68ee0cf6618a533c0bea34c35fa162c8d2
Clara Ruiz-Santacruz Jiménez <clara@MacBook-Pro-de-Clara.local>
2024-05-17 08:50:17 +0200

    working on WorkshopDetail

--- src/components/WorkshopDetail/WorkshopDetail.jsx
+++ src/components/WorkshopDetail/WorkshopDetail.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom';
+import './WorkshopDetail.css';
 
 const WorkshopDetail = () => {
 
   const location = useLocation();
   const talleres = location.state.talleres;
+  console.log(talleres);
 
   return (
 
     <>
-    <h1>ESTO ES WorkshopDetail</h1>^
-    <p> </p>talleres
+      <div className="workshopdetail-body">
+        <h1>ESTO ES WorkshopDetail</h1>^
+
+        <div className='workshop-image-container'>
+          <img
+            src={talleres.Imagen}
+            alt={talleres.Titulo}
+          />
+        </div>
+
+        <div className="workshop-details-container">
+          <div className="workshop-date-and-schedule-container">
+            <p className="workshop-date">{talleres.Fecha}</p>
+            <p className="workshop-schedule">{talleres.Horario}</p>
+          </div>
+          <h3 className="workshop-title">{talleres.Titulo}</h3>
+          <p className="workshop-description">{talleres.Descripción}</p>
+        </div>
+      </div>
+
+
+
     </>
   )
 }
