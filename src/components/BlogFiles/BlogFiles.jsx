1196b4acc4269822de390453c45d4fa045aa7f75
Lidia <ufonicap@gmail.com>
2024-05-20 14:20:15 +0200

    blogfiles update

--- src/components/BlogFiles/BlogFiles.jsx
+++ src/components/BlogFiles/BlogFiles.jsx
@@ -2,28 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './BlogFiles.css';
 
 const BlogFiles = () => {
-  const [blogs, setBlogs] = useState([]);
-    const [articulos, setArticulos] = useState([]);
-    useEffect(() => {
-      const fetchArticulos = async () => {
-        try {
-          const response = await BlogHandler.getAllBlogs();
-          setArticulos(response);
-        } catch (error) {
-          console.error("Error al cargar los artículos de blog:", error);
-        }
-      };
-      fetchArticulos();
-    }, []);
-  
+  const [articulos, setArticulos] = useState([]);
+
+  useEffect(() => {
+    const fetchArticulos = async () => {
+      try {
+        // Realiza la llamada a tu endpoint "articulo" para obtener los datos
+        const response = await fetch('URL_DEL_ENDPOINT/articulo');
+        const data = await response.json();
+        setArticulos(data);
+      } catch (error) {
+        console.error("Error al cargar los artículos de blog:", error);
+      }
+    };
+    fetchArticulos();
+  }, []);
+
   return (
     <div className='blogfiles-content'>
       <h1>Archivos</h1>
-      {blogs.map(blog => (
-        <div key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>{blog.content}</p>
-          <p>{new Date(blog.date).toLocaleDateString()}</p>
+      {articulos.map(articulo => (
+        <div key={articulo.id}>
+          <h2>{articulo.title}</h2>
+          <p>{articulo.content}</p>
+          <p>{new Date(articulo.date).toLocaleDateString()}</p>
         </div>
       ))}
     </div>
