c6f2cdb23a3a6154f6aab1e95738ebd6c7fb1744
Roger German Cecconello <nia.luna12@gmail.com>
2024-05-21 09:47:18 +0200

    modified blogcard component

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./BlogCard.css";
-
 const BlogCard = ({ blogs }) => {
   if (!blogs || blogs.length === 0) {
     return <div></div>;
   }
-
   return (
-    <div className="product-container">
-      {blogs.map((blog, index) => (
-        <div key={index} className="card">
-          <Link to={`/articulos/${blog.ID_Articulo}`} state={{ article: blog }}>
-            <div className="product-image-container">
-              <img src={blog.Imagen} alt={blog.Titulo} />
-              <div className="overlay">Leer más</div>
+    <div className='container-blog'>
+      <div className="product-container">
+        {blogs.map((blog, index) => (
+          <div key={index} className="card">
+            <img className='blog-icon-blog' src="/img/blogicon.svg" alt="calendar-icon" />
+            <Link to={`/articulo/${blog.ID_Articulo}`} state={{ article: blog }}>
+              <div className='product-image-container'>
+                <img className='blog-img'
+                  src={blog.Imagen}
+                  alt={blog.Titulo}
+                />
+                <div className="overlay">
+                  Leer más
+                </div>
+              </div>
+            </Link>
+            <div className="product-details">
+              <h3 className="product-title">{blog.Titulo}</h3>
+              <div className='date-calendar-style-blog'>
+                <img className='calendar-icon-blog-size' src="/img/calendar-icon.svg" alt="calendar-icon" />
+                <p className="blog-date">{blog.Fecha}</p>
+              </div>
+              <p className="blog-content">{blog.Contenido}</p>
             </div>
-          </Link>
-          <div className="product-details">
-            <h3 className="product-title">{blog.Titulo}</h3>
-            <p className="blog-date">{blog.Fecha}</p>
-            <p className="blog-content">{blog.Contenido}</p>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
-
 export default BlogCard;
