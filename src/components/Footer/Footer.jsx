6eef1da1052e3bcdb5bdfa05fb9139fbecc8b14e
elisacl <elisacapellinlopez@gmail.com>
2024-05-24 12:49:21 +0200

    first footer commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <>
-   <h1>FOOTER</h1>
-   </>
-  )
-}
+    <footer>
+      <div className="footer-container">
+        <div className="contact-info">
+          <p><i className="fas fa-map-marker-alt"></i> Calle Palas Atenea, 31, Almería</p>
+          <p><i className="fas fa-phone-alt"></i> +34 675 281 352</p>
+          <p><i className="fas fa-envelope"></i> dime@migueldecastro.org</p>
+        </div>
+        <div className="social-icons">
+          <img src="/img/Linkedin.svg" alt="LinkedIn" />
+          <img src="/img/Facebook.svg" alt="Facebook" />
+          <img src="/img/Instagram.svg" alt="Instagram" />
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
