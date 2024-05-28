fb5df8f6f63ebb3c852947bf968615e0f632e60c
Blanca <blankdgonzfigue@gmail.com>
2024-05-28 12:48:02 +0200

    puerto en login cambio a 5000

--- src/views/privateViews/AdminLoginView.jsx
+++ src/views/privateViews/AdminLoginView.jsx
@@ -12,7 +12,7 @@ const AdminLoginView = () => {
    
     try {
      
-      const response = await axios.post('http://127.0.0.1:5001/api/login', userData);
+      const response = await axios.post('http://127.0.0.1:5000/api/login', userData);
   
       
       if (response.data.token) {
